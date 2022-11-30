bd9f3196422bd8162658ba87858879d7d103c2fb
Mohamed Abouelnaga <mu7ammad.majed@gmail.com>
2022-11-30 23:19:12 +0100

    loaded new data

--- archs.py
+++ archs.py
@@ -33,9 +33,9 @@ class ConvNet(Module):
 ###################################################################################3
 
 class MyFCN(FCN):
-    def __init__(self, c_in, c_out, layers=[16,16], kss=[3,2]):
+    def __init__(self, c_in, c_out, layers=[16,16], kss=[5,3]):
         assert len(layers) == len(kss)
-        self.convblock1 = ConvBlock(c_in, layers[0], kss[0],padding=(1,))
+        self.convblock1 = ConvBlock(c_in, layers[0], kss[0],padding=(0,))
         self.convblock2 = ConvBlock(layers[0], layers[1], kss[1],padding=(0,))
         # self.convblock3 = ConvBlock(layers[1], layers[2], kss[2],padding=(0,))
 
