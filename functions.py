bda497c746ff6105e33a7eb63d0a29afddfc90e6
Mohamed Abouelnaga <mu7ammad.majed@gmail.com>
2022-12-22 10:20:44 +0100

    added train and predict for autoencoder

--- functions.py
+++ functions.py
@@ -1,6 +1,61 @@
 from imports import *
 
 # ****************************
+# predict autoencoder *****************
+# *******************************
+def predict_autoencoder(model, test_dls):
+  predictions, losses = [], []
+  criterion = nn.MSELoss().to(device)
+  with torch.no_grad():
+    model = model.eval()
+    for seq_true in test_dls:
+      seq_true = seq_true[0].to(device)
+      seq_pred = model(seq_true)
+      loss = criterion(seq_pred, seq_true)
+      predictions.append(seq_pred)
+      losses.append(loss.item())
+    predictions = torch.cat(predictions).detach().cpu().numpy()
+  return predictions, losses
+# ****************************
+# train autoencoder *****************
+# *******************************
+def train_autoencoder(model, train_dls, val_dls, n_epochs,lr = 1e-3):
+  optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+  criterion = nn.MSELoss().to(device)
+  history = dict(train=[], val=[])
+  best_model_wts = deepcopy(model.state_dict())
+  best_loss = 10000.0
+  for epoch in range(1, n_epochs + 1):
+    model = model.train()
+    train_losses = []
+    for seq_true in train_dls:
+      optimizer.zero_grad()
+      seq_true = seq_true[0].to(device)
+      seq_pred = model(seq_true)
+      loss = criterion(seq_pred, seq_true)
+      loss.backward()
+      optimizer.step()
+      train_losses.append(loss.item())
+    val_losses = []
+    model = model.eval()
+    with torch.no_grad():
+      for seq_true in val_dls:
+        seq_true = seq_true[0].to(device)
+        seq_pred = model(seq_true)
+        loss = criterion(seq_pred, seq_true)
+        val_losses.append(loss.item())
+    train_loss = np.mean(train_losses)
+    val_loss = np.mean(val_losses)
+    history['train'].append(train_loss)
+    history['val'].append(val_loss)
+    if val_loss < best_loss:
+      best_loss = val_loss
+      best_model_wts = deepcopy(model.state_dict())
+    print(f'Epoch {epoch}: train loss {train_loss} val loss {val_loss}')
+  model.load_state_dict(best_model_wts)
+  return model.eval(), history
+
+# ****************************
 # weighted normalized kullback leibler divergence *****************
 # *******************************
 def normalized_kl(p,q,weight = 0.1): #normalized KL
