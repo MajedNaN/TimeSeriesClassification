6005e467a8acb2e8e72edf29876aa70b247bbd22
Mohamed Abouelnaga <mu7ammad.majed@gmail.com>
2023-02-01 13:26:36 +0100

    updated .gitignore

--- scripts/run_main_add_missing_data.sh
+++
@@ -1,2 +0,0 @@
-RUN_ARGS="--data_path ../data --file_name df_minimal.csv --save_path ../data --window_size 120 --min_diff_multip 1.5 --max_diff_multip 3.2"
-python main_add_missing_data.py $RUN_ARGS
